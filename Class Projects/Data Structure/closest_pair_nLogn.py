3c26148c225e7d32b5157c5bf855b9e9993d56ba
ChuckTirtasaputra <101743291+ChuckTirtasaputra@users.noreply.github.com>
2025-05-01 09:33:41 -0400

    Update closest_pair_nLogn.py

--- Class Projects/Data Structure/closest_pair_nLogn.py
+++ Class Projects/Data Structure/closest_pair_nLogn.py
@@ -1,100 +1,80 @@
-# Closest Point O(n log n) 
-import math
+# Chuck Tirtasaputra
+# closest points - n log n solution
 
-class Point:
-	def __init__(self, x, y):
-		self.x = x
-		self.y = y
+# sample data (points and solution) 
+# P = [(9,2),(2,7),(1,8),(4,3),(5,1),(7,4),(8,6),(3,5),(6,9)] 
+# S = (((2, 7), (1, 8)), 1.4142135623730951)
 
-def dist(p1, p2):
-    return math.sqrt((p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2)
-
-def bruteForce(ax):
-	mi = dist(ax[0], ax[1])
-	p1 = ax[0]
-	p2 = ax[1]
-	ln_ax = len(ax)
-	if ln_ax == 2:
-		return p1, p2, mi
-	for i in range(ln_ax-1):
-		for j in range(i + 1, ln_ax):
-			if i != 0 and j != 1:
-				d = dist(ax[i], ax[j])
-				if d < mi:  # Update min_dist and points
-					mi = d
-					p1, p2 = ax[i], ax[j]
-	return p1, p2, mi
-	
-def closestPair(ax, ay):
-	n = len(ax)
-	print(ax)
-	if n <= 3:
-		bruteForce(ax)
-
-	# split list in half
-	mid = n // 2
-	Lx = ax[:mid]
-	Rx = ax[mid:]
-
-	midpoint = ax[mid][0] 
-	Ly = list()
-	Ry = list()
-
-	for x in ay:  # split ay into 2 arrays using midpoint
-		if x[0] <= midpoint:
-			Ly.append(x)
-		else:
-			Ry.append(x)
-	
-	# Call recursively both arrays after split
-	(p1, q1, mi1) = closestPair(Lx, Ly)
-	(p2, q2, mi2) = closestPair(Rx, Ry)
-
-	# Determine smaller distance between points of 2 arrays
-	if mi1 <= mi2:
-		dis = mi1
-		min = (p1, q1)
-	else:
-		dis = mi2
-		min = (p2, q2)
-
-	# Call function to account for points on the boundary
-
-	(p3, q3, mi3) = closestSplitPair(ax, ay, dis, min)
-
-	# Determine smallest distance for the array
-
-	if dis <= mi3:
-		return min[0], min[1], dis
-	else:
-		return p3, q3, mi3
-
-def closestSplitPair(p_x, p_y, delta, best_pair):
-    len_x = len(p_x)  # store length - quicker
-    max_x = p_x[len_x // 2][0]  # select midpoint on x-sorted array
-    # Create a subarray of points not further than delta from
-    # midpoint on x-sorted array
-    s_y = [x for x in p_y if max_x - delta <= x[0] <= max_x + delta]
-    best = delta  # assign best value to delta
-    len_y = len(s_y)  # store length of subarray for quickness
-    for i in range(len_y - 1):
-        for j in range(i+1, min(i + 7, len_y)):
-            p, q = s_y[i], s_y[j]
-            dst = dist(p, q)
-            if dst < best:
-                best_pair = p, q
-                best = dst
-    return best_pair[0], best_pair[1], best
-
-def solution(P):
-    ax = sorted(P, key=lambda P: P[0])  # Presorting x
-    ay = sorted(P, key=lambda P: P[1])  # Presorting y
-    p1, p2, mi = closestPair(ax, ay)  # Recursive D&C function
-    return p1,p2, mi
+from math import ceil, dist
 
 P = [(9,2),(2,7),(1,8),(4,3),(5,1),(7,4),(8,6),(3,5),(6,9)]
 
-ax = sorted(P, key=lambda P: P[0])  # Presorting x
-n = len(ax)
-#print(ax[1])
-solution(P)
+def X(p):
+	return p[0]
+
+def Y(p):
+	return p[1]
+
+Px = sorted(P,key=X)
+Py = sorted(P,key=Y)
+
+# solution using Python Lists (inefficient)
+def find_closest_pair(Px,Py):
+	if len(Px) < 2: # list too short
+		return ((None,None),float("inf"))
+	elif len(Px) == 2: # only one pair
+		return ((Px),dist(Px[0],Px[1]))
+	elif len(Px) == 3: # best of three
+		d1 = dist(Px[0],Px[1])
+		d2 = dist(Px[0],Px[2])
+		d3 = dist(Px[1],Px[2])
+		if d1 <= d2 and d1 <= d3:
+			return ((Px[0],Px[1]),d1)
+		elif d2 <= d1 and d2 <= d3:
+			return ((Px[0],Px[2]),d2)
+		else:
+			return ((Px[1],Px[2]),d3)
+	else: # more than three, so solve recursively
+		mid = ceil(len(Px)/2)
+		Qx = Px[:mid]  # left side
+		Rx = Px[mid:]  # right side
+		Qy = Py[:mid]  # left side
+		Ry = Py[mid:]  # right side
+		Qpair = find_closest_pair(Qx,Qy)  # closest pair on left
+		Rpair = find_closest_pair(Rx,Ry)  # closest pair on right
+		delta = min(Qpair[1],Rpair[1])    # smallest distance of the two
+
+		# find max x in Q (on left side)
+		xmax = float("-inf")
+		for i in Qx + Qy:
+			if i[0] > xmax:
+				xmax = i[0]
+
+		# make list of points near (within delta) the max x vertical line
+		S = []
+		# TODO
+		Lx = X(Qx[-1])
+		for i in P:
+			if abs(Lx - X(i)) < delta:
+				S.append(i)
+		Sy = sorted(S, key=Y)
+		
+		# find closest pair of points near the mid line
+		Spair = ((None,None),float("inf"))
+		# TODO
+		for i in range(len(Sy)-1):
+			for j in range(i+1, i+16):
+				if dist(Sy[i%len(Sy)], Sy[j%len(Sy)]) < Spair[1] and dist(Sy[i%len(Sy)], Sy[j%len(Sy)]) != 0:
+					Spair = ((Sy[i%len(Sy)], Sy[j%len(Sy)]), dist(Sy[i%len(Sy)], Sy[j%len(Sy)]))
+
+		#returning the closest pair
+		if Spair[1] < Qpair[1] and Spair[1] < Rpair[1]:
+			return Spair
+		elif Rpair[1] < Qpair[1] and Rpair < Spair[1]:
+			return Rpair
+		else:
+			return Qpair
+		#i%len(S)
+		# choose best of Spair, Qpair, and Rpair
+
+print(find_closest_pair(Px,Py))
